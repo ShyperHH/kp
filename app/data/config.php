f247e8ac3630e74ff478ceaec5e7fda40345de45
Denis Belov <syperpolsovatel555@mail.ru>
2024-01-13 12:40:29 +0300

    kp

--- app/data/config.php
+++ app/data/config.php
@@ -1,9 +1,9 @@
 <?php
 /*Подключение к БД*/
-const DB_HOST = 'db4.myarena.ru';                    // The host/ip to your SQL server
-const DB_USER = 'u32868_sb';                        // The username to connect with
-const DB_PASS = 'CaYona1415';                        // The password
-const DB_NAME = 'u32868_planner';                        // Database name
+const DB_HOST = '';                    // The host/ip to your SQL server
+const DB_USER = '';                        // The username to connect with
+const DB_PASS = '';                        // The password
+const DB_NAME = '';                        // Database name
 
 /*Кол-во дней для предупреждения пользователя*/
 const limitDays=7;
